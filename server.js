32b9754147aeff02771c170e45543481abcbb0b5
izzy-2023 <izzyzinxhirija@gmail.com>
2023-12-16 20:25:33 -0500

    Magic 8 Ball express application created

--- server.js
+++ server.js
@@ -49,6 +49,28 @@ app.get('/tip/:total/:tipPercentage', (req, res) => {
     res.json({ tip: tipAmount.toFixed(2) });
   });
 
+// *********************************
+// MAGIC 8 BALL ROUTES
+// *********************************
+
+const responses = [
+    "It is certain", "It is decidedly so", "Without a doubt", "Yes definitely",
+    "You may rely on it", "As I see it yes", "Most likely", "Outlook good",
+    "Yes", "Signs point to yes", "Reply hazy try again", "Ask again later",
+    "Better not tell you now", "Cannot predict now", "Concentrate and ask again",
+    "Don't count on it", "My reply is no", "My sources say no",
+    "Outlook not so good", "Very doubtful"
+  ];
+  
+  app.get('/magic/:question', (req, res) => {
+    const question = req.params.question.replace(/%20/g, ' '); // Replace %20 with spaces
+    const randomResponse = responses[Math.floor(Math.random() * responses.length)];
+    
+    const htmlResponse = `<h1>Your Question: ${question}</h1><h1>8 Ball Response: ${randomResponse}</h1>`;
+    res.send(htmlResponse);
+  });
+
+
 
 // *****************************
 // TURNING ON SERVER LISTENER
